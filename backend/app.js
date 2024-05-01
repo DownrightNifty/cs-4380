5075ce150d5855087aa3b4212e25a3a64d5d9c04
Timothy Harrington <116909945+TimHarrington999@users.noreply.github.com>
2024-04-30 21:03:28 -0500

    initial db setup

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const PostModel = require('./Models/post');
+
+mongoose.connect("mongodb+srv://server:1FzjhXJCPYfb2SJJ@recipeexchange.1y5gdq4.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to database');
+})
+.catch(() => {
+  console.log("Database connection error");
+})
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,7 +23,7 @@ app.use((req, res, next) => {
 
 //console.log('what');
 
-app.use('/api/posts', (req, res, next) => {
+/* app.use('/api/posts', (req, res, next) => {
     //console.log('Hello please answer');
     const posts = [
         {
@@ -26,6 +37,27 @@ app.use('/api/posts', (req, res, next) => {
         message: "This is fetched data",
         posts: posts
     });
+}); */
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new PostModel({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
+  console.log(post);
+  res.status(201).json({
+    message:"Post added successful"
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  PostModel.find().then(documents => {
+    res.status(200).json({
+      message:"This is fetched data",
+      posts: documents
+    });
+  });
 });
 
 module.exports = app;
